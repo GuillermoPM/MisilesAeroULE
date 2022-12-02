35c46680afa35bf4195ba5646af64ba72bcef542
Alejandro-PazR <apazro00@estudiantes.unileon.es>
2022-12-02 16:58:28 +0100

    Added intro

--- Script Lanzador.py
+++ Script Lanzador.py
@@ -1,1 +1,10 @@
+"""
+Script para los cálculos del segundo ejercicio entregable de la asignatura de Vehículos Lanzadores y Misiles.
+
+@Autores: Guillermo Peña Martínez, Alejandro Paz Rodríguez, Raúl Ordás Collado
+@Fecha: 2/12/2022
+@Referencias: 
+"""
+
 import numpy as np
+
