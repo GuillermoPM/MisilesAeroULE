342338e55b6256d1f893ba54a27083a80a3efe01
Alejandro-PazR <apazro00@estudiantes.unileon.es>
2022-12-02 21:13:40 +0100

    aaa

--- Script Lanzador.py
+++ Script Lanzador.py
@@ -7,4 +7,24 @@ Script para los cálculos del segundo ejercicio entregable de la asignatura de V
 """
 
 import numpy as np
+import pandas as pd
 
+datos_etapas = {
+	"impulso especifico": [315, 330, 345],
+	"fraccion estructural": [0.10, 0.15, 0.20]
+}
+
+etapas = pd.DataFrame(datos_etapas, index=["etapa 1", "etapa 2", "etapa 3"])
+
+g0 = 0.00981	# Aceleración gravitatoria (m/s^2)
+mf = 2000		# Carga útil (kg)
+vbo = 7.8		# Velocidad en el apagado (km/s)
+
+Is = 420#etapas["impulso especifico"].mean()
+ε = 0.15#etapas["fraccion estructural"].mean()
+
+
+""" Primera parte """
+
+πPL = (np.exp((-vbo)/(Is*g0)) - ε)/(1-ε)
+print(πPL)
